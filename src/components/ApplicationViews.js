8ecdbda6b21ae80810b4218c1219c23316498e66
Connor FitzGerald <connorreecefitzgerald@gmail.com>
2019-03-06 12:03:29 -0500

    kennel now runs off a database

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -7,58 +7,50 @@ import OwnerList from './owner/OwnerList'
 
 
 class ApplicationViews extends Component {
-    employeesFromAPI = [
-        { id: 1, name: "Jessica Younker" },
-        { id: 2, name: "Jordan Nelson" },
-        { id: 3, name: "Zoe LeBlanc" },
-        { id: 4, name: "Blaise Roberts" }
-    ]
-
-    locationsFromAPI = [
-        { id: 1, name: "Nashville North", address: "500 Circle Way" },
-        { id: 2, name: "Nashville South", address: "10101 Binary Court" }
-    ]
-
-    animals = [
-        { id: 1, name: "Doodles" },
-        { id: 2, name: "Jack" },
-        { id: 3, name: "Angus" },
-        { id: 4, name: "Henley" },
-        { id: 5, name: "Derkins" },
-        { id: 6, name: "Checkers" }
-    ]
-
-    owners = [
-        { id: 1, name: "Ryan Tanay", phone: "23162136" },
-        { id: 2, name: "Emma Beaton", phone: "21379893071" },
-        { id: 3, name: "Dani Adkins", phone: "4577329437" },
-        { id: 4, name: "Adam Oswalt", phone: "8797798098" },
-        { id: 5, name: "Fletcher Bangs", phone: "76769866" },
-        { id: 6, name: "Angela Lee", phone: "8908908089" }
-    ]
-
-    ownedAnimals = []
 
     state = {
-        employees: this.employeesFromAPI,
-        locations: this.locationsFromAPI,
-        animals: this.animals,
-        owners: this.owners
+        employees: [],
+        locations: [],
+        animals: [],
+        owners: [],
+        ownedAnimals: []
+    };
+    componentDidMount() {
+        const newState = {};
+        fetch("http://localhost:5002/employees")
+            .then(employees => employees.json())
+            .then(parsedEmployees => {
+                newState.employees = parsedEmployees;
+                return fetch("http://localhost:5002/locations")
+            }).then(locations => locations.json())
+            .then(parsedlocations => {
+                newState.locations = parsedlocations;
+                return fetch("http://localhost:5002/animals")
+            }).then(animals => animals.json())
+            .then(parsedanimals => {
+                newState.animals = parsedanimals;
+                return fetch("http://localhost:5002/owners")
+            }).then(owners => owners.json())
+            .then(parsedOwners => {
+                newState.owners = parsedOwners;
+                this.setState(newState)
+            })
     }
 
     render() {
         return (
-            <div className= "container-div">
+            <div className="container-div">
                 <Route exact path="/" render={(props) => {
                     return <LocationList locations={this.state.locations} />
                 }} />
                 <Route path="/animals" render={(props) => {
-                    return <AnimalList animals={this.state.animals} />
+                    return <AnimalList animals={this.state.animals}
+                        owners={this.state.owners} ownedAnimals={this.state.ownedAnimals} />
                 }} />
                 <Route path="/employees" render={(props) => {
                     return <EmployeeList employees={this.state.employees} />
                 }} />
-                 <Route path="/owners" render={(props) => {
+                <Route path="/owners" render={(props) => {
                     return <OwnerList owners={this.state.owners} />
                 }} />
             </div>
